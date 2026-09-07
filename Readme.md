44fa59ec8bf4e7e9e5904b799bf5f8dfbd53a4c5
name <thevikramaditya200514@gmail.com>
2026-09-08 02:27:20 +0530

    connect db

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 # My Chai or Backend Learning Journy
+cluster password = rboAzhNdT9Po8g1x 
---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose  from "mongoose";
+import {DB_NAME} from '../constants.js';
+
+const connectDB = async () =>{
+    try {
+        const connectionInstace = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`MongoDB connected !! DB HOST: ${connectionInstace.connection.host}` );
+        
+        console.log(process.platform);
+        
+    } catch (error) {
+        console.error('MONGODB connection FAILD ', error)
+        process.exit(1)
+    }
+}
+export default connectDB
